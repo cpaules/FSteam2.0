84e7047e8bd4b9072e67f4110e6c61bc7be75da8
Chase Paules <cpaules@gmail.com>
2017-10-19 20:02:00 +0000

    added link to user show page in header

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   resources :ratings
   resources :games
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  get '/users/:id' => 'users#show'
+  get '/users/:id' => 'users#show', as: 'user'
   post '/games/:id/add' => 'games#add', as: 'add_game'
   get '/users/:user_id/games/:id' => 'users#game_show', as: 'user_game'
   get '/users/:user_id/games/:id/edit' => 'ratings#edit', as: 'user_rating'
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'application#home'
